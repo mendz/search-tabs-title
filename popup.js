39236c45403823511e5994baacc73cf1fd6b519f
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2018-08-17 00:49:14 +0300

    combine the search types to show them in the same time

--- popup.js
+++ popup.js
@@ -1,47 +1,37 @@
 /* globals chrome */
 
 let titleTabsCurrentWindow = [];
+const stripHtmlTags = htmlString => htmlString.replace(/<([^>]+)+>/g, '$1');
 
 function focusTab() {
   chrome.tabs.update(Number(this.dataset.id), {
-    active: true
+    active: true,
   });
-}
-
-function getSearchFilterType() {
-  return Array.from(document.querySelectorAll('div#radio-input-filter input')).find(input => input.checked).dataset.type;
 }
 
 function loadListItems(items, regex) {
   const list = document.querySelector('ul#list-search-tab-title-results');
-  const checkedFilterType = getSearchFilterType();
 
-  list.innerHTML = items.map(tab => {
-    const { title, url } = tab;
+  list.innerHTML = items
+    .map(tab => {
+      const { tabIndex } = tab;
+      let { title: itemTitleText, url: itemUrlText } = tab;
 
-    let itemTitle;
-    let itemText;
+      itemTitleText = stripHtmlTags(itemTitleText);
+      itemUrlText = stripHtmlTags(itemUrlText);
 
-    switch (checkedFilterType) {
-    case 'url':
-      itemTitle = title;
-      itemText = url;
-      break;
-    default:
-      itemTitle = url;
-      itemText = title;
-      break;
-    }
+      if (regex) {
+        const matchQueryTitle = itemTitleText.match(regex) ? itemTitleText.match(regex)[0] : itemTitleText;
+        const matchQueryUrl = itemUrlText.match(regex) ? itemUrlText.match(regex)[0] : itemUrlText;
+        itemTitleText = itemTitleText.replace(regex, `<span class="matchQuery">${matchQueryTitle}</span>`);
+        itemUrlText = itemUrlText.replace(regex, `<span class="matchQuery">${matchQueryUrl}</span>`);
+      }
 
-    if (regex) {
-      const matchQuery = itemText.match(regex)[0];
-      itemText = itemText.replace(regex, `<span class="matchQuery">${matchQuery}</span>`);
-    }
-
-    return `
-    <li data-id="${tab.id}" title="${itemTitle}">${itemText}</li>
+      return `
+    <li data-id="${tab.id}" title="Tab index: ${tabIndex}"><span class="item-title-text">${itemTitleText}</span><span class="item-url-text">${itemUrlText}</span></li>
     `;
-  }).join('');
+    })
+    .join('');
 
   document.querySelectorAll('ul#list-search-tab-title-results li').forEach(item => item.addEventListener('click', focusTab));
 
@@ -50,45 +40,41 @@ function loadListItems(items, regex) {
 }
 
 function initListItems() {
-  chrome.tabs.query({
-    currentWindow: true
-  }, tabsArray => {
-    const tabsTitles = tabsArray.map(tabWindow => {
-      const info = {
-        id: tabWindow.id,
-        url: tabWindow.url,
-        title: tabWindow.title
-      };
-      return info;
-    });
+  chrome.tabs.query(
+    {
+      currentWindow: true,
+    },
+    tabsArray => {
+      const tabsTitles = tabsArray.map(tabWindow => {
+        const info = {
+          id: tabWindow.id,
+          url: tabWindow.url,
+          title: tabWindow.title,
+          tabIndex: tabWindow.index + 1
+        };
+        return info;
+      });
 
-    titleTabsCurrentWindow = tabsTitles;
-    loadListItems(tabsTitles);
-  });
+
+      titleTabsCurrentWindow = tabsTitles;
+      loadListItems(tabsTitles);
+    }
+  );
 }
 
 function onChangeInputSearch() {
   const inputValue = this.value;
-  const searchFilterType = getSearchFilterType();
 
   if (inputValue.length !== 0) {
     const regex = new RegExp(inputValue, 'gi');
-    const filteredTabs = titleTabsCurrentWindow.filter(tab => regex.test(tab[searchFilterType]));
+    const filteredTabsByTitle = titleTabsCurrentWindow.filter(tab => regex.test(tab.title));
+    const filteredTabsByUrl = titleTabsCurrentWindow.filter(tab => regex.test(tab.url));
 
-    loadListItems(filteredTabs, regex);
+    const setFilteredTabs = Array.from(new Set([...filteredTabsByTitle, ...filteredTabsByUrl]));
+    loadListItems(setFilteredTabs, regex);
   } else {
     loadListItems(titleTabsCurrentWindow);
   }
-}
-
-function onChangeInputFilterType() {
-  const inputEvent = new Event('input', {
-    bubbles: true,
-    cancelable: true
-  });
-
-  const searchInput = document.querySelector('input#search-tab-title');
-  searchInput.dispatchEvent(inputEvent);
 }
 
 function setInputListener() {
@@ -96,8 +82,6 @@ function setInputListener() {
   input.addEventListener('input', onChangeInputSearch);
 
   input.focus();
-
-  document.querySelectorAll('div#radio-input-filter input').forEach(inputType => inputType.addEventListener('change', onChangeInputFilterType));
 }
 
 document.addEventListener('DOMContentLoaded', () => {

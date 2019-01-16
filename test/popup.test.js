f64839974a63121959357b0eb7e5c5744ab49dd9
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-01-16 15:53:41 +0200

    WIP:  tests pass, changed to script module  - need to fix duplicated file and test place

--- test/popup.test.js
+++ test/popup.test.js
@@ -1,48 +1,48 @@
-const rewire = require('rewire');
-const faker = require('faker');
-
-// jest.mock('../popup.js');
-const popupJs = rewire('../popup.js');
-const loadListItems = popupJs.__get__('loadListItems');
-
+import faker from 'faker';
+import { loadListItems } from './popupFunctions.js';
 
 describe('load list items', () => {
-   it('should load items from parameter and populate DOM ', () => {
-
-      document.body.innerHTML = `
-      <h1 id="main-title">Search Your Tab</h1>
-      <div id="main-container">
-        <div id="input-container">
-          <input id="search-tab-title" name="search-tab-title" type="search" placeholder="Insert title/URL" />
-          <h4 id="count-items" title="Search item count"></h4>
-        </div>
-        <ul id="list-search-tab-title-results"></ul>
+   document.body.innerHTML = `
+   <h1 id="main-title">Search Your Tab</h1>
+   <div id="main-container">
+      <div id="input-container">
+         <input id="search-tab-title" name="search-tab-title" type="search" placeholder="Insert title/URL" />
+         <h4 id="count-items" title="Search item count"></h4>
       </div>
-      `;
+      <ul id="list-search-tab-title-results"></ul>
+   </div>
+   `;
 
-      const items = [
-         {
-            id: faker.random.uuid(),
-            url: faker.internet.url(),
-            title: faker.lorem.words(),
-            tabIndex: faker.random.number({ min: 1, max: 30 }) + 1,
-            active: true
-         },
-         {
-            id: faker.random.uuid(),
-            url: faker.internet.url(),
-            title: faker.lorem.words(),
-            tabIndex: faker.random.number({ min: 1, max: 30 }) + 1,
-            active: false
-         }
-      ];
+   const items = [
+      {
+         id: faker.random.uuid(),
+         url: faker.internet.url(),
+         title: faker.lorem.words(),
+         tabIndex: faker.random.number({ min: 1, max: 30 }) + 1,
+         active: true
+      },
+      {
+         id: faker.random.uuid(),
+         url: faker.internet.url(),
+         title: faker.lorem.words(),
+         tabIndex: faker.random.number({ min: 1, max: 30 }) + 1,
+         active: false
+      }
+   ];
+
+
+   it('should load items from parameter and populate DOM', () => {
 
       loadListItems(items);
 
-      //   const list = document.querySelector('ul#list-search-tab-title-results');
+      expect(document.querySelector('ul#list-search-tab-title-results li')).toBeTruthy;
+      expect(document.querySelectorAll('ul#list-search-tab-title-results li').length).toEqual(2);
+   });
 
-      expect( document.querySelector('ul#list-search-tab-title-results li').length).toEqual(2);
+   it('should update header count items', () => {
 
+      loadListItems(items);
 
+      expect(document.querySelector('h4#count-items').textContent).toEqual('2');
    });
 });
